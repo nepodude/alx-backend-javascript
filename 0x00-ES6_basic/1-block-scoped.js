c0d436eea6f24844d27f907649e183f96469db8a
nepodude <nepo.jean73@gmail.com>
2025-05-03 13:08:54 +0200

    updated task1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,13 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-    let task = true;  // scoped only inside the if block
-    let task2 = false;
-    return [task, task2];  // use these if condition is true
+    task = true;
+    task2 = false;
   }
 
-  return [task, task2];  // use outer values if false
+  return [task, task2];
 }
-  
