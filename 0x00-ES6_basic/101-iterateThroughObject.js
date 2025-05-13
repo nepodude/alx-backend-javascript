1a16c12c729538adbc6299b67701b71c5406bbf2
nepodude <nepo.jean73@gmail.com>
2025-05-13 10:27:28 +0200

    updated

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,7 @@
+export default function iterateThroughObject(reportWithIterator) {
+  const names = [];
+  for (const name of reportWithIterator) {
+    names.push(name);
+  }
+  return names.join(' | ');
+}
