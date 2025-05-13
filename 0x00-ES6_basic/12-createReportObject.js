35c100a9e83b81d412e3a93c486dba93007f0dea
nepodude <nepo.jean73@gmail.com>
2025-05-13 10:10:19 +0200

    updated

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,11 +1,11 @@
 export default function createReportObject(employeesList) {
-    return {
-  // allEmployees maps department names to arrays of employee names
-  allEmployees: { ...employeesList },
+  return {
+    // allEmployees maps department names to arrays of employee names
+    allEmployees: { ...employeesList },
 
-  // Method to get the number of departments
-  getNumberOfDepartments(allEmployees) {
-    return Object.keys(allEmployees).length;
-  }
+    // Method to get the number of departments
+    getNumberOfDepartments(allEmployees) {
+      return Object.keys(allEmployees).length;
+    },
   };
 }
