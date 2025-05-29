a1cd6910dfe5f3362bad26a3b8732fa878726b22
Nepomunezero <j.munezero1@alustudent.com>
2025-05-29 23:33:58 +0200

    updated the file

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,12 +2,13 @@ import { signUpUser } from './4-user-promise';
 import { uploadPhoto } from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise
-    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((res) => (
-      res.map((o) => ({
-        status: o.status,
-        value: o.status === 'fulfilled' ? o.value : String(o.reason),
-      }))
-    ));
+  const results = await Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]);
+
+  return results.map((o) => ({
+    status: o.status,
+    value: o.status === 'fulfilled' ? o.value : o.reason,
+  }));
 }
