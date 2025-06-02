5d0b72c0ff17db806902ab399825f688b2fa5612
Nepomunezero <j.munezero1@alustudent.com>
2025-06-02 11:22:31 +0200

    updated file 1

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,4 +1,4 @@
 import ClassRoom from './0-classroom';
 function initializeRooms() {
-  return [ClassRoom(19), ClassRoom(20), ClassRoom(34)];
+  return([ClassRoom(19), ClassRoom(20), ClassRoom(34)]);
 }
