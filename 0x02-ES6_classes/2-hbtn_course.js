88b1c4f7e392309b9d3b65174a56e2f8f9ff3188
Nepomunezero <j.munezero1@alustudent.com>
2025-06-02 11:45:29 +0200

    updated file 1

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,16 @@
+export default class ALXCourse {
+  constructor(name, length, students) {
+    if(typeof(name) !== 'String'){
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof(length) !== Number){
+      throw new TypeError('Length must be a number');
+    }
+    if(!Array.isArray(students) || !students.every(s => typeof s === 'string')){
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+}
