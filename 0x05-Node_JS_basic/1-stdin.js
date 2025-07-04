e74a3c27c3ea6593de475cfa7dfaff75bb5b4f94
Nepomunezero <j.munezero1@alustudent.com>
2025-07-04 17:54:39 +0200

    debugged

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
-process.stdout.write('Welcome to Holberton school, what is your name?\n');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (data) => {
+process.stdin.on('data', () => {
   const name = data.toString().trim();
   process.stdout.write(`Your name is: ${name}\n`);
 });
