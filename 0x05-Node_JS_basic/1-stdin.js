6ae2ca61a1134206ea03460008c183f561b982ce
Nepomunezero <j.munezero1@alustudent.com>
2025-07-04 17:31:35 +0200

    initiated

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,4 +1,4 @@
-process.stdout.write('Welcome to ALX, what is your name?\n');
+process.stdout.write('Welcome to Holbert School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
