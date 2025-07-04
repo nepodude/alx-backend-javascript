edec6cf37f5c99c88fb1caed233997a342d55b3e
Nepomunezero <j.munezero1@alustudent.com>
2025-07-04 17:55:34 +0200

    debugged

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,13 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', () => {
-  const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
 });
 
-// Handle when input ends (piped input or Ctrl+D)
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
